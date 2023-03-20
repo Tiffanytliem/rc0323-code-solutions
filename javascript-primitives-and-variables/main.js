8b5afadca701fcec9d25a3bb8732e340cf2779f4
Tiffany Liem <thuytliem@gmail.com>
2023-03-20 18:10:05 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+const fullName = 'Tiffany Liem';
+const isCool = true;
+const totalPets = 1;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('type of unknown:', typeof unknown);
+
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
