56c8044bd456cdc1955e996f1e0f72f1ea7d21a5
Tiffany Liem <thuytliem@gmail.com>
2023-03-20 21:19:38 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 20;
+const height = 25;
+const area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 90;
+const payment = 100;
+const change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 90;
+const midterm = 80;
+const final = 86;
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Tiffany';
+const lastName = 'Liem';
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 8;
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 92;
+const isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
