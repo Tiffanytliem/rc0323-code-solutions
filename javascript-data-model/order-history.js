1777a4f8ef96f9103970b4dfea55512c1cebb4fa
Tiffany Liem <thuytliem@gmail.com>
2023-03-21 19:35:54 +0000

    Updated order-history.js

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,39 +1,50 @@
 var orderHistory = [
   {
-    item: 'JavaScript for impatient programmers',
+    items: [
+      {
+        name: 'JavaScript for impatient programmers',
+        total: 34.00
+      }
+    ],
     datePlaced: 'August 4, 2020',
     dateDelivered: 'August 8, 2020',
-    total: 34.00,
-    orderNumber: 11439416898772232
-  },
-  {
-    item: 'The Timeless Way of Building',
-    datePlaced: 'July 19, 2020',
-    dateDelivered: 'July 20, 2020',
-    total: 41.33,
-    // eslint-disable-next-line no-loss-of-precision
-    orderNumber: 11399842681280257
-  },
-  {
-    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    datePlaced: 'July 4, 2020',
-    dateDelivered: 'July 7, 2020',
-    total: 15.98,
-    orderNumber: 11328831772648248
+    orderNumber: '114-3941689-8772232'
   },
   {
     items: [
       {
-        item: 'GameCube Controller - Super Smash Bors. Edition (Nintendo Switch)',
+        name: 'The Timeless Way of Building',
+        total: 41.33
+      }
+    ],
+    datePlaced: 'July 19, 2020',
+    dateDelivered: 'July 20, 2020',
+    orderNumber: '113-9984268-1280257'
+  },
+  {
+    items: [
+      {
+        name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        total: 15.98
+      }
+    ],
+    datePlaced: 'July 4, 2020',
+    dateDelivered: 'July 7, 2020',
+    orderNumber: '113-2883177-2648248'
+  },
+  {
+    items: [
+      {
+        name: 'GameCube Controller - Super Smash Bors. Edition (Nintendo Switch)',
         total: 94.95
       },
       {
-        item: 'The Art of Sql',
+        name: 'The Art of Sql',
         total: 33.99
       }
     ],
     datePlaced: 'July 3, 2020',
     dateDelivered: 'July 5, 2020',
-    orderNumber: 11328831772648248
+    orderNumber: '113-2883177-2648248'
   }
 ];
