fc36e93f2de54073fb9258eac9ae3f33c798410b
Tiffany Liem <thuytliem@gmail.com>
2023-03-23 21:43:49 +0000

    Completed array-manipulation-1 exercise.

---
+++ array-manipulation-1/src/reverse.js
@@ -0,0 +1,7 @@
+/* exported reverse */
+function reverse(array) {
+  const reversedArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    reversedArray.push(array[i]);
+  } return reversedArray;
+}
