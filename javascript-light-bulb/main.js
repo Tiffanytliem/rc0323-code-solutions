b16efe7d00dfdb8a5667b8766a346eb1d3cc3386
Tiffany Liem <thuytliem@gmail.com>
2023-04-04 18:05:21 +0000

    Completed javascript-light-bulb challenge

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,15 +1,13 @@
 
-const element = document.querySelector('.lightbulb.off');
+const element = document.querySelector('.lightbulb');
 
 function lightSwitch(event) {
   if (document.querySelector('.lightbulb.off')) {
     element.className = 'lightbulb on';
     document.querySelector('.container.off').className = 'container on';
-    console.log(element);
   } else {
     element.className = 'lightbulb off';
     document.querySelector('.container.on').className = 'container off';
-    console.log(element);
   }
 }
 
